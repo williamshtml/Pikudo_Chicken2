f00f436bddf13a77150476aaba42bd5d8d2c17e9
Williams <willixms2004@gmail.com>
2026-06-08 18:12:34 -0500

    fix: se agrego el exclude de bd y security y se configuro la clase principal

--- src/main/java/com/restaurant/PikudoChicken2Application.java
+++ src/main/java/com/restaurant/PikudoChicken2Application.java
@@ -2,12 +2,17 @@ package com.restaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    SecurityAutoConfiguration.class, 
+    DataSourceAutoConfiguration.class
+})
 public class PikudoChicken2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PikudoChicken2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PikudoChicken2Application.class, args);
+    }
 
 }
