1e992a9f01646977cf4c14267396671d29dfdc53
Sebastian Benjamin <noreply>
2026-06-24 17:53:13 -0500

    Añadiendo codigo para la clase de Categoria.java

--- src/main/java/com/pikudo/entity/Categoria.java
+++ src/main/java/com/pikudo/entity/Categoria.java
@@ -1,13 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.pikudo.entity;
 
-/**
- *
- * @author rodri
- */
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Entity
+@Table(name = "categorias")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class Categoria {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    /*
+    @Id: Define que la variable sera la clave primaria (PK)
+    @GeneratedValue: Configura columna como autoincrementaria
+    */
+
+    @NotBlank(message = "El nombre de la categoría es obligatorio")
+    @Column(unique = true, nullable = false, length = 60)
+    private String nombre;
+    /*
+    @NotBlank: Valida que el nombre de la categoria no llegue vacio. De ser asi, se frena la operacion y salta el mensaje indicado
+    @Column: No se permite usernames duplicados / No espacios vacios / Limite de caracteres: 60
+    */
+
+    @Builder.Default
+    @Column(nullable = false)
+    private Boolean estado = true;
+    /*
+    @Builder.Default: Ayuda a @Builder a reconocer por defecto el estado indicado
+    @Column: No espacios vacios.
+    */
 }
