33218fe7744d0598b8caf976d5e83f807544d459
Sebastian Benjamin <noreply>
2026-06-24 18:07:39 -0500

    Añadiendo codigo enum para la clase de EstadoPedido y TipoComprobante

--- src/main/java/com/pikudo/entity/EstadoPedido.java
+++ src/main/java/com/pikudo/entity/EstadoPedido.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.pikudo.entity;
 
-/**
- *
- * @author rodri
- */
-public class EstadoPedido {
-    
+public enum EstadoPedido {
+    PENDING,     // El mozo tomó el pedido pero aún no se envía a cocina
+    IN_KITCHEN,  // El pedido ya está siendo preparado por los cocineros
+    PAID,        // El cliente ya pagó la cuenta en caja
+    CANCELLED    // El pedido fue cancelado (por error de digitación, falta de stock, etc.)
 }
