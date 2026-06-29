de3160a002e6f4218f1db6d7920f72a3568c3608
Sebastian Benjamin <noreply>
2026-06-29 18:45:31 -0500

    Añadiendo codigo a RestTemplateConfig

--- src/main/java/com/pikudo/config/RestTemplateConfig.java
+++ src/main/java/com/pikudo/config/RestTemplateConfig.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.pikudo.config;
 
-/**
- *
- * @author willi
- */
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import java.time.Duration;
+
+@Configuration
+
 public class RestTemplateConfig {
-    
+    /*
+     Configura y expone el Bean de RestTemplate con políticas de tiempo de espera rigurosas.
+     Garantiza que las consultas externas de RUC/DNI no congelen el sistema de la pollería.
+     */
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                // 1. Tiempo de espera para establecer la conexión inicial con la API externa (3 segundos)
+                .setConnectTimeout(Duration.ofSeconds(3))
+                
+                // 2. Tiempo de espera máximo para recibir los datos de la respuesta (5 segundos)
+                .setReadTimeout(Duration.ofSeconds(5))
+                
+                .build();
+    }
 }
