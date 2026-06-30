93026a1855ca72fd8f1a9129c2d1cadfe64fd0a1
Sebastian Benjamin <noreply>
2026-06-30 17:53:24 -0500

    Codigo en la linea 6 y 12 para la Auditoria (fechas de creacion/modificacion)

--- src/main/java/com/restaurant/PikudoChicken2Application.java
+++ src/main/java/com/restaurant/PikudoChicken2Application.java
@@ -3,11 +3,13 @@ package com.restaurant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.restaurant", "com.pikudo"})
 @EnableJpaRepositories(basePackages = "com.pikudo.repository")
 @EntityScan(basePackages = "com.pikudo.entity")
+@EnableJpaAuditing
 
 public class PikudoChicken2Application {
 
